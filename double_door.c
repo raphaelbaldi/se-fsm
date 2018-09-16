ef73b2d1dad2564d88ba8847037ebe9ea0a159ea
Raphael Baldi <raphaelbaldi@gmail.com>
2018-09-16 13:55:50 -0300

    Reviewed version of double doors with more states

--- double_door.c
+++ double_door.c
@@ -5,12 +5,19 @@
 typedef void *(*state_func)();
 
 int external_door = 0, internal_door = 0, client_between_doors = 0;
-int client_wait = 0, c;
+int client_action = 0, c;
 
 int kbhit(void);
 void *idle();
 void *entering_bank();
 void *exiting_bank();
+void *open_internal_door();
+void *close_internal_door();
+void *open_external_door();
+void *close_external_door();
+void *client_enters_doors();
+void *client_exits_doors();
+
 
 #define ENTER_BANK 1
 #define EXIT_BANK 2
@@ -20,74 +27,98 @@ void *idle() {
 	external_door = 0; internal_door = 0; client_between_doors = 0;
 
   // If no client is waiting, keep the bank doors idle
-	if (client_wait == 0)	return idle;
+	if (client_action == 0)	return idle;
 
   // If a client is entering the bank, switch state to enter
-	if (client_wait == ENTER_BANK)	return entering_bank;
+	if (client_action == ENTER_BANK)	return entering_bank;
 
   // If a client is exiting the bank, switch state to exit
-	if (client_wait == EXIT_BANK)	return exiting_bank;
+	if (client_action == EXIT_BANK)	return exiting_bank;
+	return 0;
+}
+
+void *client_enters_doors() {
+	fprintf(stderr, "CLIENT ENTERING DOORS ");
+	client_between_doors = 1;
+	if (client_action == ENTER_BANK) {
+		return close_external_door;
+	} else if (client_action == EXIT_BANK) {
+		return close_internal_door;
+	}
+	return 0;
+}
+
+void *client_exits_doors() {
+	fprintf(stderr, "CLIENT EXITING DOORS ");
+	client_between_doors = 0;
+	if (client_action == ENTER_BANK) {
+		return close_internal_door;
+	} else if (client_action == EXIT_BANK) {
+		return close_external_door;
+	}
+	return 0;
+}
+
+void *open_internal_door() {
+	fprintf(stderr, "OPEN INTERNAL DOOR ");
+	internal_door = 1;
+	if (client_action == ENTER_BANK) {
+		return client_exits_doors;
+	} else if (client_action == EXIT_BANK) {
+		return client_enters_doors;
+	}
+	return 0;
+}
+
+void *close_internal_door() {
+	fprintf(stderr, "CLOSE INTERNAL DOOR ");
+	internal_door = 0;
+	if (client_action == ENTER_BANK) {
+		client_action = 0;
+		return idle;
+	} else if (client_action == EXIT_BANK) {
+		return open_external_door;
+	}
+	return 0;
+}
+
+void *open_external_door() {
+	fprintf(stderr, "OPEN EXTERNAL DOOR ");
+	external_door = 1;
+	if (client_action == ENTER_BANK) {
+		return client_enters_doors;
+	} else if (client_action == EXIT_BANK) {
+		return client_exits_doors;
+	}
+	return 0;
+}
+
+void *close_external_door() {
+	fprintf(stderr, "CLOSE EXTERNAL DOOR ");
+	external_door = 0;
+	if (client_action == ENTER_BANK) {
+		return open_internal_door;
+	} else if (client_action == EXIT_BANK) {
+		client_action = 0;
+		return idle;
+	}
 	return 0;
 }
 
 void *entering_bank() {
 	fprintf(stderr, "ENTERING ");
-  if (client_between_doors) {
-    if (external_door) {
-      // Client in between doors, external door open, close it
-      external_door = 0;
-    } else if (!internal_door) {
-      // Client in between doors, internal door closed, open it
-      internal_door = 1;
-    } else {
-      // Internal door open, client exit
-      client_between_doors = 0;
-    }
-  } else {
-    if (internal_door) {
-      // No client in between doors, internal door open, close and get back to idle
-      internal_door = 0;
-      client_wait = 0;
-      return idle;
-    } else if (!external_door) {
-      // Client not in between doors, external door closed, open it
-      external_door = 1;
-    } else {
-      // Client not in between doors, external door open, client enters it
-      client_between_doors = 1;
-    }
+  if (!client_between_doors) {
+    return open_external_door;
   }
-  return entering_bank;
+  return 0;
 }
 
 void *exiting_bank() {
 	fprintf(stderr, "EXITING ");
-  if (client_between_doors) {
-    if (internal_door) {
-      // Client in between doors, internal door open, close it
-      internal_door = 0;
-    } else if (!external_door) {
-      // Client in between doors, external door closed, open it
-      external_door = 1;
-    } else {
-      // Client in between doors, external door open, client exit it
-      client_between_doors = 0;
-    }
-  } else {
-    if (external_door) {
-      // No client inside, external door open, close and get back to idle
-      external_door = 0;
-      client_wait = 0;
-      return idle;
-    } else if (!internal_door) {
-      // Both doors closed, open internal door
-      internal_door = 1;
-    } else {
-      // Internal door open, client enters it
-      client_between_doors = 1;
-    }
-  }
-  return exiting_bank;
+  if (!client_between_doors) {
+		return open_internal_door;
+	}
+	return 0;
 }
 
 int main() {
@@ -95,14 +126,14 @@ int main() {
 
 	while(1){
 		curr_state = (state_func)(*curr_state)();
-		fprintf(stderr, "BANK: (external door: %d | client between doors: %d | internal door: %d)\n", external_door, client_between_doors, internal_door);
+		fprintf(stderr, "(external door: %d | client between doors: %d | internal door: %d)\n", external_door, client_between_doors, internal_door);
 		sleep(1);
-		if (kbhit() && !client_wait){
+		if (kbhit() && !client_action){
 			c = getchar();
 			if (c == '0') {
-				client_wait = ENTER_BANK;
+				client_action = ENTER_BANK;
       } else if (c == '1') {
-        client_wait = EXIT_BANK;
+        client_action = EXIT_BANK;
       }
 		}
 	}
